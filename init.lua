4262ea24740bbce42092b0cd268111de74d3190b
Riccardo <delucariccardo@hotmail.com>
2025-07-06 01:28:09 +0200

    first commit

---
+++ init.lua
@@ -0,0 +1,15 @@
+hs.hotkey.bind({"cmd"}, "1", function()
+    hs.application.launchOrFocus("Safari")
+end)
+
+hs.hotkey.bind({"cmd"}, "2", function()
+  hs.application.launchOrFocus("Terminal")
+end)
+
+hs.hotkey.bind({"cmd"}, "3", function()
+  hs.application.launchOrFocus("Visual Studio Code")
+end)
+
+hs.hotkey.bind({"cmd"}, "4", function()
+  hs.application.launchOrFocus("DBeaver")
+end)
