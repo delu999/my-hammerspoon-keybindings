17fc99f40d48c4da83b2007fe08f8a8779767e37
delu999 <150914753+delu999@users.noreply.github.com>
2025-07-27 15:57:51 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 hs.hotkey.bind({"cmd"}, "1", function()
-    hs.application.launchOrFocus("Safari")
+    hs.application.launchOrFocus("Brave Browser")
 end)
 
 hs.hotkey.bind({"cmd"}, "2", function()
-  hs.application.launchOrFocus("Terminal")
+  hs.application.launchOrFocus("Ghostty")
 end)
 
 hs.hotkey.bind({"cmd"}, "3", function()
-  hs.application.launchOrFocus("Visual Studio Code")
+  hs.application.launchOrFocus("Zed")
 end)
 
 hs.hotkey.bind({"cmd"}, "4", function()
@@ -18,22 +18,24 @@ hs.hotkey.bind({"cmd"}, "5", function()
   hs.application.launchOrFocus("T3chat") -- installed as a PWA
 end)
 
+-- THIS IS NOT NECESSARY. Keyboard shortcuts > Keyboard > Move focus to next window
+
 -- change between windows of the same app
-hs.hotkey.bind({"cmd"}, "\\", function()
-    local win = hs.window.focusedWindow()
-    if not win then return end
-    local app = win:application()
-    if not app then return end
-    local windows = hs.fnutils.filter(app:allWindows(), function(w)
-        return w:isStandard() and w:isVisible()
-    end)
-    if #windows <= 1 then return end
-
-    table.sort(windows, function(a, b)
-        return a:id() < b:id()
-    end)
-
-    local currentIndex = hs.fnutils.indexOf(windows, win)
-    local nextIndex = (currentIndex % #windows) + 1
-    windows[nextIndex]:focus()
-end)
+-- hs.hotkey.bind({"cmd"}, "\\", function()
+--    local win = hs.window.focusedWindow()
+--    if not win then return end
+--    local app = win:application()
+--    if not app then return end
+--    local windows = hs.fnutils.filter(app:allWindows(), function(w)
+--        return w:isStandard() and w:isVisible()
+--    end)
+--    if #windows <= 1 then return end
+--
+--    table.sort(windows, function(a, b)
+--        return a:id() < b:id()
+--    end)
+--
+--    local currentIndex = hs.fnutils.indexOf(windows, win)
+--    local nextIndex = (currentIndex % #windows) + 1
+--  windows[nextIndex]:focus()
+-- end)
